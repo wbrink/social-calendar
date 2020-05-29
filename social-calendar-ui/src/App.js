da0c1c507ac9433a5cad316414d3b6f983963c35
wbrink <will.brink55@gmail.com>
2020-05-29 09:54:39 -0700

    added friends route

--- social-calendar-ui/src/App.js
+++ social-calendar-ui/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import LoginSignup from "./pages/LoginSignup/LoginSignup.js"
 import Login from "./pages/Login/Login.js"
 import Signup from "./pages/Signup/Signup.js"
+import ViewFriends from "./components/ViewFriends";
+
 function App() {
   
   return (
@@ -11,6 +13,7 @@ function App() {
     <Route exact path="/LoginSignup" component={LoginSignup} />
     <Route exact path="/Login" component={Login} />
     <Route exact path="/Signup" component={Signup} />
+    <Route exact path="/friends" component={ViewFriends} />
     </Router>
   );
 }
