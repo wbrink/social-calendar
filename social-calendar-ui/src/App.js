279837d4472efdd255a0a1c478df5ad21fe38525
wbrink <will.brink55@gmail.com>
2020-05-31 00:07:05 -0700

    logged user in with setUser and made redirect using props from react-router-dom

--- social-calendar-ui/src/App.js
+++ social-calendar-ui/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./pages/Login/Login.js"
 import SignUp from "./pages/Signup/Signup.js"
 import UserContext from "./UserContext.js"
+import ViewFriends from "./pages/ViewFriends";
+
 function App() {
   // create logged in provider
   const [user, setUser] = useState(false); // by defualt false
@@ -12,6 +14,7 @@ function App() {
       <UserContext.Provider value={{user, setUser}}>
         <Route exact path="/Login" component={Login} />
         <Route exact path="/SignUp" component={SignUp} />
+        <Route exact path="/friends" component={ViewFriends} />
       </UserContext.Provider>
     </Router>
   );
