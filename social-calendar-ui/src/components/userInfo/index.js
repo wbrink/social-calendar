180adce2aeccc0cbe85242c490b0510fd827e301
Dorian Young <DorianYoung@Ymail.com>
2020-06-05 11:39:39 -0700

    USer info component complete

--- social-calendar-ui/src/components/userInfo/index.js
+++ social-calendar-ui/src/components/userInfo/index.js
@@ -5,18 +5,59 @@ import {
   Route,
   useParams,
 } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import "./style.css";
 
 export default function UserInfo(props) {
   let { user } = useParams();
 
   return (
     <div id="main">
-      <div class="row">username {props.state.user.username}</div>
-      <div class="row">Edit Profile Button</div>
-      <div class="row">Location {props.state.user.location}</div>
-      <div class="row">Bio {props.state.user.bio}</div>
-      <div class="row"> Date Created {props.state.user.createdAt}</div>
-      <div class="row">Friends {props.state.user.friends.length}</div>
+      <div class="row" id="rowcss">
+        <div class="col-3">
+          <img id="profilepic" src="https://via.placeholder.com/90" />
+        </div>
+        <div class="col-1"></div>
+        <div class="col-8">
+          <center>
+            <p class="text-uppercase">
+              <h4>username {props.state.user.username}</h4>
+            </p>
+          </center>
+          <Button variant="secondary" size="sm" block>
+            Edit Profile
+          </Button>
+        </div>
+      </div>
+
+      <div class="row" id="rowcss">
+        <center>
+          <small>
+            <strong>
+              <em>Location {props.state.user.location}</em>
+            </strong>
+          </small>{" "}
+        </center>
+      </div>
+      <div class="row" id="rowcss">
+        This is the bio section. This is the coolest app ever and i just need to
+        fill it with a bio {props.state.user.bio}
+      </div>
+
+      <div class="row" id="rowcss1">
+        <div class="col-4">
+          <a href="/friends"> Friends {props.state.user.friends.length}</a>
+        </div>
+        <div class="col-2"></div>
+        <div class="col-6">
+          <a href="/friends">
+            {" "}
+            <small>
+              <strong>Date Created {props.state.user.createdAt}</strong>
+            </small>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
