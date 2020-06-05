d96599a9d0e98244fa5ba6ab2e33f581948faba9
wbrink <will.brink55@gmail.com>
2020-06-05 11:29:15 -0700

    seed file created (still use user a to login as this has the most friends and friend requests)

--- seed.js
+++ seed.js
@@ -16,26 +16,31 @@ mongoose_db.on("error", function(err) {
 })
 
 
-mongoose_db.once("open",function() {
-  var date = new Date.now();
-  var a = new db.User({username: "a", password: "123", friends: [{_id: Bob._id, date: date}, {_id: Elaine._id, date: date}, {_id: Ernie._id, date: date}, {_id: Bert._id, date: date}, {_id: Billy._id, date: date}], bio: "test user for us to use", location: ""})
-  var Bob = new db.User({username: "Bob", password: "123", friends: [{_id: Bob._id, date: date}, {_id: Elaine._id, date: date}, {_id: Ernie._id, date: date}, {_id: Bob._id, date: date}]})
-  var Billy = new db.User({username: "Billy", password: "123", friends: [{_id: Bob._id, date: date}, {_id: Elaine._id, date: date}, {_id: Ernie._id, date: date}, {_id: Bob._id, date: date}]});
-  var Julia = new db.User({username: "Julia", password: "123", friends: [{_id: Bob._id, date: date}, {_id: Elaine._id, date: date}, {_id: Ernie._id, date: date}, {_id: Bob._id, date: date}]})
-  var Kramer = new db.User({username: "Kramer", password: "123", friends: [{_id: Bob._id, date: date}, {_id: Elaine._id, date: date}, {_id: Ernie._id, date: date}, {_id: Bob._id, date: date}]})
-  var Michael = new db.User({username: "Michael", password: "123", friends: [{_id: Bob._id, date: date}, {_id: Elaine._id, date: date}, {_id: Ernie._id, date: date}, {_id: Bob._id, date: date}]})
-  var Jason = new db.User({username: "Jason", password: "123", friends: [{_id: Bob._id, date: date}, {_id: Elaine._id, date: date}, {_id: Ernie._id, date: date}, {_id: Bob._id, date: date}]})
-  var George = new db.User({username: "George", password: "123", friends: [{_id: Bob._id, date: date}, {_id: Elaine._id, date: date}, {_id: Ernie._id, date: date}, {_id: Bob._id, date: date}]})
-  var Jerry = new db.User({username: "Jerry", password: "123", friends: [{_id: Bob._id, date: date}, {_id: Elaine._id, date: date}, {_id: Ernie._id, date: date}, {_id: Bob._id, date: date}]})
-  var Elaine = new db.User({username: "Elaine", password: "123", friends: [{_id: Bob._id, date: date}, {_id: Elaine._id, date: date}, {_id: Ernie._id, date: date}, {_id: Bob._id, date: date}]})
-  var Elton = new db.User({username: "Elton", password: "123", friends: [{_id: Bob._id, date: date}, {_id: Elaine._id, date: date}, {_id: Ernie._id, date: date}, {_id: Bob._id, date: date}]})
-  var Ernie = new db.User({username: "Ernie", password: "123", friends: [{_id: Bob._id, date: date}, {_id: Elaine._id, date: date}, {_id: Ernie._id, date: date}, {_id: Bob._id, date: date}]})
-  var Bernie = new db.User({username: "Bernie", password: "123", friends: [{_id: Bob._id, date: date}, {_id: Elaine._id, date: date}, {_id: Ernie._id, date: date}, {_id: Bob._id, date: date}]})
-  var Bert = new db.User({username: "Bert", password: "123", friends: [{_id: Bob._id, date: date}, {_id: Elaine._id, date: date}, {_id: Ernie._id, date: date}, {_id: Bob._id, date: date}]})
-  var Fred = new db.User({username: "Fred", password: "123", friends: [{_id: Bob._id, date: date}, {_id: Elaine._id, date: date}, {_id: Ernie._id, date: date}, {_id: Bob._id, date: date}]})
-  var Bo = new db.User({username: "Fred", password: "123", friends: [{_id: Bob._id, date: date}, {_id: Elaine._id, date: date}, {_id: Ernie._id, date: date}, {_id: Bob._id, date: date}]})
-  var Bella = new db.User({username: "Bella", password: "123", friends: [{_id: Bob._id, date: date}, {_id: Elaine._id, date: date}, {_id: Ernie._id, date: date}, {_id: Bob._id, date: date}]})
-  var Blake = new db.User({username: "Blake", password: "123", friends: [{_id: Bob._id, date: date}, {_id: Elaine._id, date: date}]});
+mongoose_db.once("open", async function() {
+  var date = Date.now();
+
+  var a = new db.User({username: "a", password: "123", friends: [], bio: "test user for us to use", location: "Central Valley, CA"})
+
+  var Bob = new db.User({username: "Bob", password: "123", friends: [{_id: a._id, date: date}], bio: "test bio for bob", location: "New York"})
+  var Billy = new db.User({username: "Billy", password: "123", friends: [{_id: a._id, date: date}], bio: "test bio for billy", location: "New York"});
+  var Julia = new db.User({username: "Julia", password: "123", friends: [], bio: "test bio for Julia", location: "New York"})
+  var Kramer = new db.User({username: "Kramer", password: "123", friends: [], bio: "test bio Kramer", location: "New York"})
+  var Michael = new db.User({username: "Michael", password: "123", friends: [], bio: "test bio Michael", location: "New York"})
+  var Jason = new db.User({username: "Jason", password: "123", friends: [], bio: "test bio Jason", location: "New York"})
+  var George = new db.User({username: "George", password: "123", friends: [], bio: "test bio George", location: "New York"})
+  var Jerry = new db.User({username: "Jerry", password: "123", friends: [], bio: "test bio Jerry", location: "New York"})
+  var Elaine = new db.User({username: "Elaine", password: "123", friends: [{_id: a._id, date: date}], bio: "test bio Elaine", location: "New York"})
+  var Elton = new db.User({username: "Elton", password: "123", friends: [], bio: "test bio Elton", location: "New York"})
+  var Ernie = new db.User({username: "Ernie", password: "123", friends: [{_id: a._id, date: date}], bio: "test bio Ernie", location: "New York"})
+  var Bernie = new db.User({username: "Bernie", password: "123", friends: [], bio: "test bio Bernie", location: "New York"})
+  var Bert = new db.User({username: "Bert", password: "123", friends: [{_id: a._id, date: date}], bio: "test bio Bert", location: "New York"})
+  var Fred = new db.User({username: "Fred", password: "123", friends: [], bio: "test bio Fred", location: "New York"})
+  var Bo = new db.User({username: "Bo", password: "123", friends: [], bio: "test bio Bo", location: "New York"})
+  var Bella = new db.User({username: "Bella", password: "123", friends: [], bio: "test bio Bella", location: "New York"})
+  var Blake = new db.User({username: "Blake", password: "123", friends: [], bio: "test bio Blake", location: "New York"});
+
+  a.friends = [{_id: Bob._id, date: date}, {_id: Elaine._id, date: date}, {_id: Ernie._id, date: date}, {_id: Bert._id, date: date}, {_id: Billy._id, date: date}];
+
   
   var req1 = new db.FriendRequest({from: Kramer._id, to: a._id})
   var req2 = new db.FriendRequest({from: Bo._id, to: a._id})
@@ -47,38 +52,47 @@ mongoose_db.once("open",function() {
 
 
   // save the documents
-  a.save((err) => {console.log(err)});
-  Bob.save((err) => {console.log(err)});
-  Billy.save((err) => {console.log(err)});
-  Julia.save((err) => {console.log(err)});
-  Kramer.save((err) => {console.log(err)});
-  Michael.save((err) => {console.log(err)});
-  Jason.save((err) => {console.log(err)});
-  George.save((err) => {console.log(err)});
-  Jerry.save((err) => {console.log(err)});
-  Elaine.save((err) => {console.log(err)});
-  Elton.save((err) => {console.log(err)});
-  Ernie.save((err) => {console.log(err)});
-  Bernie.save((err) => {console.log(err)});
-  Bert.save((err) => {console.log(err)});
-  Fred.save((err) => {console.log(err)});
-  Bo.save((err) => {console.log(err)});
-  Bella.save((err) => {console.log(err)});
-  Blake.save((err) => {console.log(err)});
+  try { 
+    await a.save();
+    await Bob.save();
+    await Billy.save();
+    await Julia.save();
+    await Kramer.save();
+    await Michael.save();
+    await Jason.save();
+    await George.save();
+    await Jerry.save();
+    await Elaine.save();
+    await Elton.save();
+    await Ernie.save();
+    await Bernie.save();
+    await Bert.save();
+    await Fred.save();
+    await Bo.save();
+    await Bella.save();
+    await Blake.save();
 
-  req1.save((err) => {console.log(err)});
-  req2.save((err) => {console.log(err)});
-  req3.save((err) => {console.log(err)});
-  req4.save((err) => {console.log(err)});
-  req5.save((err) => {console.log(err)});
+    await req1.save();
+    await req2.save();
+    await req3.save();
+    await req4.save();
+    await req5.save();
 
-
-})
-
-mongoose_db.close((err) => {
-  if (err) {
-    console.log(err);
+    mongoose_db.close((err) => {
+      if (err) {
+        console.log("error:", error);
+      }
+      console.log("closing successfully");
+    })
+    
+  } catch (error) {
+    console.log("There was an error saving to db:", error);
+    mongoose_db.close((err) => {
+      if (err) {
+        console.log("error:", error);
+      }
+      console.log("closing successfully");
+    })
   }
-  console.log("closing connection");
+
 })
-return;
