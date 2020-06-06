b123b58e6b136b92edb90d95886bba125f15b03f
wbrink <will.brink55@gmail.com>
2020-06-06 11:57:10 -0700

    seed file with names created

--- seed.js
+++ seed.js
@@ -19,25 +19,25 @@ mongoose_db.on("error", function(err) {
 mongoose_db.once("open", async function() {
   var date = Date.now();
 
-  var a = new db.User({username: "a", password: "123", friends: [], bio: "test user for us to use", location: "Central Valley, CA"})
+  var a = new db.User({username: "a", name: "a", password: "123", friends: [], bio: "test user for us to use", location: "Central Valley, CA"})
 
-  var Bob = new db.User({username: "Bob", password: "123", friends: [{_id: a._id, date: date}], bio: "test bio for bob", location: "New York"})
-  var Billy = new db.User({username: "Billy", password: "123", friends: [{_id: a._id, date: date}], bio: "test bio for billy", location: "New York"});
-  var Julia = new db.User({username: "Julia", password: "123", friends: [], bio: "test bio for Julia", location: "New York"})
-  var Kramer = new db.User({username: "Kramer", password: "123", friends: [], bio: "test bio Kramer", location: "New York"})
-  var Michael = new db.User({username: "Michael", password: "123", friends: [], bio: "test bio Michael", location: "New York"})
-  var Jason = new db.User({username: "Jason", password: "123", friends: [], bio: "test bio Jason", location: "New York"})
-  var George = new db.User({username: "George", password: "123", friends: [], bio: "test bio George", location: "New York"})
-  var Jerry = new db.User({username: "Jerry", password: "123", friends: [], bio: "test bio Jerry", location: "New York"})
-  var Elaine = new db.User({username: "Elaine", password: "123", friends: [{_id: a._id, date: date}], bio: "test bio Elaine", location: "New York"})
-  var Elton = new db.User({username: "Elton", password: "123", friends: [], bio: "test bio Elton", location: "New York"})
-  var Ernie = new db.User({username: "Ernie", password: "123", friends: [{_id: a._id, date: date}], bio: "test bio Ernie", location: "New York"})
-  var Bernie = new db.User({username: "Bernie", password: "123", friends: [], bio: "test bio Bernie", location: "New York"})
-  var Bert = new db.User({username: "Bert", password: "123", friends: [{_id: a._id, date: date}], bio: "test bio Bert", location: "New York"})
-  var Fred = new db.User({username: "Fred", password: "123", friends: [], bio: "test bio Fred", location: "New York"})
-  var Bo = new db.User({username: "Bo", password: "123", friends: [], bio: "test bio Bo", location: "New York"})
-  var Bella = new db.User({username: "Bella", password: "123", friends: [], bio: "test bio Bella", location: "New York"})
-  var Blake = new db.User({username: "Blake", password: "123", friends: [], bio: "test bio Blake", location: "New York"});
+  var Bob = new db.User({username: "Bob", name: "Bob", password: "123", friends: [{_id: a._id, date: date}], bio: "test bio for bob", location: "New York"})
+  var Billy = new db.User({username: "Billy", name: "Billy", password: "123", friends: [{_id: a._id, date: date}], bio: "test bio for billy", location: "New York"});
+  var Julia = new db.User({username: "Julia", name: "Julia", password: "123", friends: [], bio: "test bio for Julia", location: "New York"})
+  var Kramer = new db.User({username: "Kramer", name: "Kramer", password: "123", friends: [], bio: "test bio Kramer", location: "New York"})
+  var Michael = new db.User({username: "Michael", name: "Michael", password: "123", friends: [], bio: "test bio Michael", location: "New York"})
+  var Jason = new db.User({username: "Jason", name: "Jason", password: "123", friends: [], bio: "test bio Jason", location: "New York"})
+  var George = new db.User({username: "George", name: "George", password: "123", friends: [], bio: "test bio George", location: "New York"})
+  var Jerry = new db.User({username: "Jerry", name: "Jerry", password: "123", friends: [], bio: "test bio Jerry", location: "New York"})
+  var Elaine = new db.User({username: "Elaine", name: "Elaine", password: "123", friends: [{_id: a._id, date: date}], bio: "test bio Elaine", location: "New York"})
+  var Elton = new db.User({username: "Elton", name: "Elton", password: "123", friends: [], bio: "test bio Elton", location: "New York"})
+  var Ernie = new db.User({username: "Ernie", name: "Ernie", password: "123", friends: [{_id: a._id, date: date}], bio: "test bio Ernie", location: "New York"})
+  var Bernie = new db.User({username: "Bernie", name: "Bernie", password: "123", friends: [], bio: "test bio Bernie", location: "New York"})
+  var Bert = new db.User({username: "Bert", name: "Bert", password: "123", friends: [{_id: a._id, date: date}], bio: "test bio Bert", location: "New York"})
+  var Fred = new db.User({username: "Fred", name: "Fred", password: "123", friends: [], bio: "test bio Fred", location: "New York"})
+  var Bo = new db.User({username: "Bo", name: "Bo", password: "123", friends: [], bio: "test bio Bo", location: "New York"})
+  var Bella = new db.User({username: "Bella", name: "Bella", password: "123", friends: [], bio: "test bio Bella", location: "New York"})
+  var Blake = new db.User({username: "Blake", name: "Blake", password: "123", friends: [], bio: "test bio Blake", location: "New York"});
 
   a.friends = [{_id: Bob._id, date: date}, {_id: Elaine._id, date: date}, {_id: Ernie._id, date: date}, {_id: Bert._id, date: date}, {_id: Billy._id, date: date}];
 
