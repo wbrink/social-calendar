a935ec6de97bcb0dcf3334892f9db6e623855ada
Aaron32365 <56372224+Aaron32365@users.noreply.github.com>
2020-06-09 13:05:56 -0700

    adjusted css on user calendar and main home page

--- social-calendar-ui/src/components/CalendarForm.js
+++ social-calendar-ui/src/components/CalendarForm.js
@@ -5,6 +5,7 @@ import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import DatePicker from "react-datepicker";
 import Button from "react-bootstrap/Button";
+import "./CalendarForm.css"
 import {
   addCalendar,
   editCalendar,
--- social-calendar-ui/src/components/userInfo/index.js
+++ social-calendar-ui/src/components/userInfo/index.js
@@ -30,17 +30,28 @@ export default function UserInfo(props) {
     
     <div id="main">
       <div class="row" id="rowcss">
-        <div class="col-3">
+        <div class="col-6">
           <a href="/editprofilepic"><img id="profilepic" src="https://via.placeholder.com/90" /></a>
-        </div>
-        <div class="col-1"></div>
-        <div class="col-8">
-          <center>
-            <p class="text-uppercase">
-              <h4>username {user.username}</h4>
+          <div className="row">
+          <p class="text-uppercase">
+            <strong>
+                <em id="userLocation">{user.location}</em>
+            </strong>
             </p>
+          </div>
+        </div>
+        <div class="col-6 userInfoContainer">
+          <center>
+              <div id="subHeaderUsername">{user.name}</div>
           </center>
-          <Link to={{pathname: "/editprofile", state: {...user}}} className="buttons edit-profile">Edit Profile</Link>
+          <div className="row justify-content-center">
+          <em  className=" col-11 col-md-11 col-lg-6 userDesc" >"If you think I'm available, your wrong. I'm too busy making this app..."</em>
+          </div>
+          <div className="row justify-content-center">
+            <small className="col-12">
+                <div id="userCreatedAt"> <b>Member Since:</b> <br/>{user.createdAt}</div>
+            </small>
+          </div>
           {/* <Button onto="/editProfile" variant="secondary" size="sm" block>
             Edit Profile
           </Button> */}
@@ -48,42 +59,24 @@ export default function UserInfo(props) {
       </div>
 
       <div class="row" id="rowcss">
-        <div class="col-2">
+        <div class="col-5">
           {" "}
-          <center>
-            <strong>
-              <em>Name {user.name} </em>
-            </strong>
-          </center>
         </div>
-        <div class="col-4"></div>
-        <div class="col-6">
+        <div class="col-7">
           <center>
             <small>
-              <strong>
-                <em>Location {user.location}</em>
-              </strong>
             </small>{" "}
           </center>
         </div>
       </div>
-      <div class="row" id="rowcss">
-        
-      </div>
 
       <div class="row" id="rowcss1">
-        <div class="col-4">
-          <a href="/friends"> Friends {user.friends.length}</a>
-        </div>
-        <div class="col-2"></div>
         <div class="col-6">
-          <a href="/friends">
-            {" "}
-            <small>
-              <strong>Date Created {user.createdAt}</strong>
-            </small>
-          </a>
+          <a href="/friends" className="col-12" id="userFriends"> Friends {user.friends.length}</a>
         </div>
+        <div className="col-6">
+          <Link to={{pathname: "/editprofile", state: {...user}}} className=" buttons edit-profile">Edit Profile</Link>
+          </div>
       </div>
     </div>
   );
