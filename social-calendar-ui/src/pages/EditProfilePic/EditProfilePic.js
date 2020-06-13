721eaacf72c2e0ae604e6d4ce5f83bbba6bab655
wbrink <will.brink55@gmail.com>
2020-06-13 12:42:20 -0700

    finished styling the editprofilepic

--- social-calendar-ui/src/pages/EditProfilePic/EditProfilePic.js
+++ social-calendar-ui/src/pages/EditProfilePic/EditProfilePic.js
@@ -6,50 +6,50 @@ import "./EditProfilePic.css";
 
 const EditProfilePic = (props) => {
   const { userState, setUserState } = useContext(UserContext);
+  
   // state variables to edit
-  const [name, setName] = useState(userState.name);
-  const [bio, setBio] = useState(userState.bio);
-  const [location, setLocation] = useState(userState.location);
   const [profilePic, setprofilePic] = useState(userState.profilePic);
+  const [active, setActive] = useState("") // holds the name of image
+
+  // goes to the last page the user was on
+  const goBack = () => {
+    props.history.push("/editprofile");;
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    fetch("/api/logged-in", {
+    fetch("/api/edit-profile-pic", {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        name: name,
-        bio: bio,
-        location: location,
         profilePic: profilePic,
       }),
     })
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+        setUserState( {...userState, profilePic: profilePic });
         props.history.push("/profile");
-        setUserState({
-          ...userState,
-          bio: bio,
-          location: location,
-          name: name,
-          profilePic: profilePic,
-        });
       });
   };
+
+
+  const handleClick = (e) => {
+    setprofilePic(e.target.dataset.id);
+    setActive(e.target.dataset.id); // sets the image as active on click which sets a border around the image
+  }
 
   useEffect(() => {
     isLoggedIn(userState, setUserState, props);
   }, []);
 
+  // on refresh if the userState is changed
   useEffect(() => {
-    setName(userState.name);
-    setLocation(userState.location);
-    setBio(userState.bio);
-    setprofilePic(userState.profilePic);
+    if (userState.loggedIn) {
+      setprofilePic(userState.profilePic);
+    }
   }, [userState]);
 
-  console.log("name", name, "profilePic", profilePic);
 
   return (
     <div className="container">
@@ -59,46 +59,23 @@ const EditProfilePic = (props) => {
             Edit Profile Picture
           </div>
 
-          <div className="row justify-content-center">
-            <div class="row" id="rowcss">
-              <img src={require("./images/guy.png")} id="profilepic" />
-              <img src={require("./images/girl1.png")} id="profilepic" />
-              <img src={require("./images/guy2.png")} id="profilepic" />
-            </div>
-            {/* 
-            RADIO BUTTON IF NEEDED 
-
-            <div class="row" id="rowcss">
-              <input
-                class="form-check-input"
-                type="radio"
-                name="exampleRadios"
-                id="exampleRadios1"
-                value="option1"
-                checked
-              />
-            </div> */}
-            <div class="row" id="rowcss">
-              <img src={require("./images/girl2.png")} id="profilepic" />
-              <img src={require("./images/girl3.png")} id="profilepic" />
-              <img src={require("./images/bluemonster.jpg")} id="profilepic" />
-            </div>
-
-            <div class="row" id="rowcss">
-              <img
-                src={require("./images/orangemonster.jpg")}
-                id="profilepic"
-              />
-              <img src={require("./images/flower.jpeg")} id="profilepic" />
-              <img src={require("./images/dog.jpg")} id="profilepic" />
-            </div>
+          <div className="row ">
+            <div className="row d-flex justify-content-center " id="rowcss">
+              <img src="/images/guy.png" className={active == "/images/guy.png" ? ("clicked") : ("noClick")} data-id="/images/guy.png" id="profilepic" onClick={handleClick} />
+              <img src="/images/girl1.png" className={active == "/images/girl1.png" ? ("clicked") : ("noClick")} data-id="/images/girl1.png" id="profilepic" onClick={handleClick}/>
+              <img src="/images/guy2.png" className={active == "/images/guy2.png" ? "clicked" : "noClick"} data-id="/images/guy2.png" id="profilepic" onClick={handleClick}/>
+              <img src="/images/girl2.png" className={active == "/images/girl2.png" ? "clicked" : "noClick"} data-id="/images/girl2.png" id="profilepic" onClick={handleClick}/>
+              <img src="/images/girl3.png" className={active == "/images/girl3.png" ? "clicked" : "noClick"} data-id="/images/girl3.png" id="profilepic" onClick={handleClick}/>
+              <img src="/images/bluemonster.jpg" className={active == "/images/bluemonster.jpg" ? "clicked" : "noClick"} data-id="/images/bluemonster.jpg" id="profilepic" onClick={handleClick}/>
+              <img src="/images/orangemonster.jpg" className={active == "/images/orangemonster.jpg" ? "clicked" : "noClick"} data-id="/images/orangemonster.jpg" id="profilepic" onClick={handleClick}/>
+              <img src="/images/flower.jpeg" className={active == "/images/flower.jpeg" ? "clicked" : "noClick"} data-id="/images/flower.jpeg" id="profilepic" onClick={handleClick}/>
+              <img src="/images/dog.jpg" className={active == "/images/dog.jpg" ? "clicked" : "noClick"} data-id="/images/dog.jpg" id="profilepic" onClick={handleClick}/>
+            </div> 
           </div>
 
           <div className="row justify-content-center">
-            <Link className="col-9" id="SignupLink" to="/editprofile">
-              {/*have an onclick event that runs a function that sends img to user model*/}
-              Save
-            </Link>
+            <div className="col-9 text-center" id="SignupLink" onClick={handleSubmit}>Save</div>
+            <div className="col-9 text-center" id="CancelButton" onClick={goBack}>Cancel</div>
           </div>
         </div>
       </div>
